c2ce1eb19c8e1d5fc7a4e2efe4a793e0218624f3
Ujwal Saurav <ujwalsaurav@gmail.com>
2020-10-31 02:30:14 +0530

    First commit

---
+++ build/precache-manifest.7b57fa2791994f390c132d31a8460d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dcfb1705173918db87fd9b3dd9a6ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12ec3087cc979e2e8780",
+    "url": "/static/css/main.7f6f2414.chunk.css"
+  },
+  {
+    "revision": "c22816b5142fbe1157b7",
+    "url": "/static/js/2.9a122109.chunk.js"
+  },
+  {
+    "revision": "5ac922b1a6e253946f13fa455dc8d57c",
+    "url": "/static/js/2.9a122109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ec3087cc979e2e8780",
+    "url": "/static/js/main.e93154cf.chunk.js"
+  },
+  {
+    "revision": "8ee40f9b318c3c875d87",
+    "url": "/static/js/runtime-main.67bfdc7e.js"
+  }
+]);
